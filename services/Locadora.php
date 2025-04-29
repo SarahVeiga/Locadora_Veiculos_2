b8765f5aa9c14063a0ada3022b51bd6608525e6a
SarahVeiga <sarahveiga55@gmail.com>
2025-04-29 11:22:44 -0300

    Update Locadora.php

--- services/Locadora.php
+++ services/Locadora.php
@@ -16,6 +16,40 @@ class Locadora {
         if (file_exists(ARQUIVO_JSON)){
 
             // Decodifica o arquivo JSON
+            $dados = json_decode(file_get_contents(ARQUIVO_JSON),true);
+
+            foreach ($dados as $dado){
+
+                if($dado['tipo']=== 'Carro'){
+                    $veiculo = new Carro($dado['modelo'], $dado['placa']);
+                } else {
+                    $veiculo = new Moto($dado['modelo'], $dado['placa']);
+                }
+                $veiculo->setDisponivel($dado['disponivel']);
+
+                $this->veiculos[] = $veiculo;
+            }
+        }
+    }
+
+    // Salvar veículos
+    private function salvarVeiculos(): void{
+        $dados = [];
+
+        foreach($this->veiculos as $veiculo){
+            $dados[] = [
+                'tipo' => ($veiculo instanceof Carro) ? 'Carro' :'Moto', 'modelo' => $veiculo -> getModelo(),
+                'placa' => $veiculo -> getPlaca(),
+                'disponivel' => $veiculo -> isDisponivel()
+            ];
+
+            $dir = dirname(ARQUIVO_JSON);
+
+            if (!is_dir($dir)){
+                mkdir($dir,0777, true);
+            }
+
+            file_put_contents(ARQUIVO_JSON, json_encode($dados, JSON_PRETTY_PRINT));
         }
     }
 }
